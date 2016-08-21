00218954ba8ca04e78d5d99a7b06e45b78929e95
Kent Nguyen <nguyen.dmz@gmail.com>
2016-08-22 00:27:45 +0800

    Minor UI tweaks

--- api/slack.rb
+++ api/slack.rb
@@ -72,8 +72,8 @@ namespace '/api/slack' do
         "title" => "",
         "pretext" => "Booking instructions",
         "text" => "#{@cmd} book `#{Space.first.code}` from `4pm` to `6pm` for `Meeting's purpose`\n
-  #{@cmd} book `#{Space.last.code }` from `Friday 4pm` to `6pm` for `Client Visit`\n
-  #{@cmd} book `#{Space.last.code }` `tomorrow 4pm` for `Interview` _(this will book 1 hour slot)_",
+#{@cmd} book `#{Space.last.code }` from `Friday 4pm` to `6pm` for `Client Visit`\n
+#{@cmd} book `#{Space.last.code }` `tomorrow 4pm` for `Interview` _(this will book 1 hour slot)_",
         "color" => "#CCC",
         "mrkdwn_in": ["text", "pretext"]
       }
