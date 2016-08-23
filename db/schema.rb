0302488342c414b5ef70f026995ad1b06e85a95e
Kent Nguyen <nguyen.dmz@gmail.com>
2016-08-23 09:27:21 +0800

    Add Role column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821143633) do
+ActiveRecord::Schema.define(version: 20160823011905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,13 @@ ActiveRecord::Schema.define(version: 20160821143633) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   null: false
-    t.string   "name",       null: false
+    t.string   "username",               null: false
+    t.string   "name",                   null: false
     t.string   "color"
     t.string   "avatar_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role",       default: 0
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
--- models/users.rb
+++ models/users.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :bookings, dependent: :destroy
 
+  USER = 0
+  ADMIN = 99
+
   before_validation(on: :create) do
     # Auto fill name with username
     self.name = self.username
